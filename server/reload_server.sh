8bdea045fb4b78b063ff673ee3d582d6d86acb29
Gabriel Previato de Andrade <gabriel.andrade@lnls.br>
2019-09-09 09:02:20 -0300

    Continue script if main.db is not found

--- server/reload_server.sh
+++ server/reload_server.sh
@@ -3,6 +3,6 @@ SERVER_PATH=/etc/recsync/server
 
 cd ${SERVER_PATH}
 
-rm main.db
+rm main.db || :
 sqlite3 main.db -init recceiver.sqlite3 .exit
 PYTHONPATH=$PWD twistd -r poll -n recceiver -f main.conf
